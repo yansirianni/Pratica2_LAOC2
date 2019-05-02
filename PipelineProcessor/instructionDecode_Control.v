0d06c5786751cdc37c4d99bc0c59bddb6ca62b71
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-05-02 20:18:18 -0300

    add some wires for connect the blocks

--- PipelineProcessor/instructionDecode_Control.v
+++ PipelineProcessor/instructionDecode_Control.v
@@ -1,5 +1,7 @@
-module instructionDecode_Control(
-    
-);
+module instructionDecode_Control(clock,reset,opcode_in,);
+
+    always @() begin
+        
+    end
 
 endmodule // instructionDecode_Control
--- PipelineProcessor/pipelineProcessor.v
+++ PipelineProcessor/pipelineProcessor.v
@@ -1,53 +1,61 @@
-
 module pipelineProcessor (DataIn, Reset, Clock, Dout, Daddress, W);
 
-input [19:0] DataIn;
-input Reset, Clock;
-output [19:0] Dout, Daddress;
+    input [19:0] DataIn;
+    input Reset, Clock;
+    output [19:0] Dout, Daddress;
 
-output reg W;
+    output reg W;
 
-//==================================================
-//                    Estágios
-//==================================================
+    wire jumpEnable;
+    wire [19:0] IF_ID_DataOut;
+    wire [3:0] IF_OpcodeOut;
 
-//Estágio 1
-instructionFetch IF(); //Carrega instruções da memória 
+    wire [3:0] ReadAddressRF1, ReadAddressRF2;
+    wire [3:0] ID_OpcodeOut;
+    wire [31:0] extended_signal;
 
-//Estágio 2
-instructionDecode ID(); //Decodifica a instrução e lê os registradores 
 
-//Estágio 3
-instructionExecute EX(); //Executa o cálculo se necessário
+    //==================================================
+    //                    Estágios
+    //==================================================
 
-//Estágio 4
-memoryAccess MEM(); //Leitura ou escrita na memória 
+    //Estágio 1
+    instructionFetch IF(Clock, Reset, jumpAddress, jumpEnable, Daddress); //Carrega instruções da memória 
 
-//Estágio 5
-writeBack WB(); //Escreve o resultado no banco de registradores
+    //Estágio 2
+    instructionDecode ID(IF_ID_DataOut,ReadAddressRF1,ReadAddressRF2); //Decodifica a instrução e lê os registradores 
 
-//==================================================
-//            Registradores de Pipeline
-//==================================================
+    //Estágio 3
+    instructionExecute EX(); //Executa o cálculo se necessário
 
-register_IF_ID IF_ID(); 
+    //Estágio 4
+    memoryAccess MEM(); //Leitura ou escrita na memória 
 
-register_ID_EX ID_EX();
+    //Estágio 5
+    writeBack WB(); //Escreve o resultado no banco de registradores
 
-register_EX_MEM EX_MEM();
+    //==================================================
+    //            Registradores de Pipeline
+    //==================================================
 
-register_MEM_WB MEM_WB();
+    register_IF_ID IF_ID(Clock, Reset, DataIn[19:16], DataIn, IF_ID_DataOut, IF_OpcodeOut); 
 
-//==================================================
-//                  Controladores
-//==================================================
+    register_ID_EX ID_EX(Clock,Reset,IF_OpcodeOut,extended_signal,read_data1,read_data2,dataRFOut1,dataRFOut2,ID_OpcodeOut);
 
-instructionDecode_Control ID_Control();
+    register_EX_MEM EX_MEM();
 
-instructionExecute_Control EX_Control();
+    register_MEM_WB MEM_WB();
 
-memoryAccess_Control MEM_Control();
+    //==================================================
+    //                  Controladores
+    //==================================================
 
-writeBack_Control WB_Control();
+    instructionDecode_Control ID_Control();
+
+    instructionExecute_Control EX_Control();
+
+    memoryAccess_Control MEM_Control();
+
+    writeBack_Control WB_Control();
 
 endmodule
