9f27ca4a58273743f6552f458714788ce26d5c3c
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-05-03 14:27:24 -0300

    add some wires for instructionExecute

--- PipelineProcessor/pipelineProcessor.v
+++ PipelineProcessor/pipelineProcessor.v
@@ -6,13 +6,17 @@ module pipelineProcessor (DataIn, Reset, Clock, Dout, Daddress, W);
 
     output reg W;
 
-    wire jumpEnable;
+    wire jumpEnable; // Habilita do jump no PC
     wire [19:0] IF_ID_DataOut;
     wire [3:0] IF_OpcodeOut;
 
     wire [3:0] ReadAddressRF1, ReadAddressRF2;
     wire [3:0] ID_OpcodeOut;
     wire [31:0] extended_signal;
+
+    wire [1:0] ALU_Control; // Controle da alu que esta dentro de instructionExecute
+    wire [15:0] EX_dataRFOut1, EX_dataRFOut2; // Saidas guardadas pelo registrador ID_EX
+    wire [15:0] EX_Out; // Resultado do instructionExecute
 
 
     //==================================================
@@ -26,7 +30,8 @@ module pipelineProcessor (DataIn, Reset, Clock, Dout, Daddress, W);
     instructionDecode ID(IF_ID_DataOut,ReadAddressRF1,ReadAddressRF2); //Decodifica a instrução e lê os registradores 
 
     //Estágio 3
-    instructionExecute EX(); //Executa o cálculo se necessário
+    //module instructionExecute(input	[1:0] control;input	[15:0]  opA, opB;output	reg [15:0]  result;);
+    instructionExecute EX(ALU_Control, EX_dataRFOut1, EX_dataRFOut2, EX_Out); //Executa o cálculo se necessário
 
     //Estágio 4
     memoryAccess MEM(); //Leitura ou escrita na memória 
@@ -40,7 +45,7 @@ module pipelineProcessor (DataIn, Reset, Clock, Dout, Daddress, W);
 
     register_IF_ID IF_ID(Clock, Reset, DataIn[19:16], DataIn, IF_ID_DataOut, IF_OpcodeOut); 
 
-    register_ID_EX ID_EX(Clock,Reset,IF_OpcodeOut,extended_signal,read_data1,read_data2,dataRFOut1,dataRFOut2,ID_OpcodeOut);
+    register_ID_EX ID_EX(Clock,Reset,IF_OpcodeOut,extended_signal,read_data1,read_data2,EX_dataRFOut1,EX_dataRFOut2,ID_OpcodeOut);
 
     register_EX_MEM EX_MEM();
 
