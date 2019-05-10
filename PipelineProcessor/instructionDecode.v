9013b451ccaa8427abe96058ec0292a3910345d9
Bruno H. A. Fonseca <bruno3145@gmail.com>
2019-05-10 13:09:28 -0300

    instructionDecode alignment and writeBack implemetation 'on going'

--- PipelineProcessor/instructionDecode.v
+++ PipelineProcessor/instructionDecode.v
@@ -1,14 +1,13 @@
-module instructionDecode(clock,reset,instruction,WriteAddress,ReadAddressRF1,ReadAddressRF2);
+module instructionDecode(clock,reset,instruction,ReadAddressRF1,ReadAddressRF2);
   input clock;
   input reset;
   input [19:0] instruction;
-  output reg [3:0] WriteAddress;
   output [3:0] ReadAddressRF1;
   output [3:0] ReadAddressRF2;
 
     //Verifica se o OPcode é 1100, (STORE) e faz atribuições.
     
-    ReadAddressRF1 = ((instruction[19:16]==4'b1100) ? instruction[15:12] : instruction[11:8]); //Tratamento de ReadAdresses diferenciado para Store.
-    ReadAddressRF2 = ((instruction[19:16]==4'b1100) ? instruction[11:8]  : instruction[7:4]);
+   assign ReadAddressRF1 = ((instruction[19:16]==4'b1100) ? instruction[15:12] : instruction[11:8]); //Tratamento de ReadAdresses diferenciado para Store.
+   assign ReadAddressRF2 = ((instruction[19:16]==4'b1100) ? instruction[11:8]  : instruction[7:4]);
 
 endmodule
--- PipelineProcessor/writeBack.v
+++ PipelineProcessor/writeBack.v
@@ -1,3 +1,10 @@
-module writeBack();
+module writeBack(memoryReadData,ulaResult, muxControl, writeBackOut);
+  input [19:0] memoryReadData;
+  input [19:0] ulaResult;
+  output [19:0] writeBackOut;
+  //output [19:0] writeAdress;
+
+assign writeBackOut = (muxControl) ? memoryReadData : ulaResult);
+    
 
 endmodule
