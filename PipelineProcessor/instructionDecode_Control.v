5f586effc72b697771e8e0f31c6e69a724db26b3
unknown <bruno3145@gmail.com>
2019-05-09 21:29:56 -0300

    Clock remove solution 4 instructionDecode_Control

--- PipelineProcessor/instructionDecode_Control.v
+++ PipelineProcessor/instructionDecode_Control.v
@@ -1,3 +1,36 @@
-module instructionDecode_Control();
+module instructionDecode_Control(clock,reset,instruction,WriteAddress,ReadAddressRF1,ReadAddressRF2);
+  input clock;
+  input reset;
+  input [19:0] instruction;
+  output reg [3:0] WriteAddress;
+  output reg [3:0] ReadAddressRF1;
+  output reg [3:0] ReadAddressRF2;
 
+/*  always @(posedge clock)
+  begin
+    case(instruction[19:16])
+
+      4'b1100:  // store
+      begin
+
+        ReadAddressRF1 <= instruction[15:12];
+        ReadAddressRF2 <= instruction[11:8];     
+
+      end
+
+    default:
+      begin
+
+        ReadAddressRF1 <= instruction[11:8];
+        ReadAddressRF2 <= instruction[7:4];
+
+      end
+    endcase
+  end  
+*/
+
+    ReadAddressRF1 = ((instruction[19:16]==4'b1100) ? instruction[15:12] : instruction[11:8]); //Tratamento de ReadAdresses diferenciado para Store.
+    ReadAddressRF2 = ((instruction[19:16]==4'b1100) ? instruction[11:8]  : instruction[7:4]);
+
+endmodule
 endmodule // instructionDecode_Control
