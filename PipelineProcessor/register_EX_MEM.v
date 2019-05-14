578a7b9a926fea1f3a8f89d6ac7e2397d8272432
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-05-14 18:00:27 -0300

    Fix register_EX_MEM and register_IF_ID

--- PipelineProcessor/register_EX_MEM.v
+++ PipelineProcessor/register_EX_MEM.v
@@ -1,11 +1,9 @@
-module register_EX_MEM(instruction,clock,reset,opcode,aluZERO,aluRESULT,read_data2,aluZEROout,aluRESULTout,dataRFOut2,opcode_out,instructionPropagation);
+module register_EX_MEM(clock,reset,instruction,aluZERO,aluRESULT,aluZEROout,aluRESULTout,instructionPropagation);
 
-  input clock, reset;
-  input [3:0] opcode;
-  input [19:0] instruction;
+  	input clock, reset;
+  	input [19:0] instruction;
 	input aluZERO; //zero da ULA
 	input [19:0] aluRESULT; //saida da ULA
-	input [15:0] read_data2; //saída 2 do banco de regs
 	 
 	 /** Verificar a saída da ula para as instruçoes de JUMP, 
 	 * referenciada como ula "ADD" no diagrama do figma.
@@ -13,22 +11,19 @@ module register_EX_MEM(instruction,clock,reset,opcode,aluZERO,aluRESULT,read_dat
 	 
 	output reg aluZEROout;
 	output reg [19:0] aluRESULTout;
-	output reg [19:0] dataRFOut2;
-	output reg [3:0] opcode_out; //propagacao do opcode para ser usado pelo proximo bloco
-	output [19:0] instructionPropagation;
+	output reg [19:0] instructionPropagation;
+
+
 	always @(posedge clock) begin
 		if (reset) begin
 			aluZEROout <= 1'b0;
 			aluRESULTout <= 20'b0;
-			dataRFOut2 <= 20'b0;
-			opcode_out <= 4'b0;
+			instructionPropagation <= 20'b0;
 		end else begin
 			aluZEROout <= aluZERO;
 			aluRESULTout <= aluRESULT;
-			dataRFOut2 <= read_data2;
-			opcode_out <= opcode;
+			instructionPropagation <= instruction;
 		end
 	end
-	
-assign instructionPropagation = instruction;
+
 endmodule
--- PipelineProcessor/register_IF_ID.v
+++ PipelineProcessor/register_IF_ID.v
@@ -1,22 +1,16 @@
-module register_IF_ID(clock,reset,opcode,instruction,data_out,opcode_out);
+module register_IF_ID(clock,reset,instruction,instructionPropagation);
 
-  input clock, reset;
-	input [3:0] opcode; //esta redundante de proposito para caso mude a estrutura do instruction
+  	input clock, reset;
 	input [19:0] instruction;
 	
-	output reg [19:0] data_out;
-	output reg [3:0] opcode_out; //propagacao do opcode para ser usado pelo proximo bloco
-	output [19:0] instructionPropagation;
+	output reg [19:0] instructionPropagation;
 
 	always @(posedge clock)
 	begin
 		if (reset) begin
-			data_out <= 20'b0;
-			opcode_out <= 4'b0;
+			instructionPropagation <= 20'b0;
 		end else begin
-			data_out <= instruction;
 			opcode_out <= opcode;
 		end
 	end
-	assign instructionPropagation = instruction;
 endmodule
