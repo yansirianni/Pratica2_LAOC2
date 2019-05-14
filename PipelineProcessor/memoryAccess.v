67d435762563bb803321a4c1f769512dfb18c386
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-05-14 17:31:30 -0300

    Add memoryAccess_Control and remeve memory_main from memoryAccess

--- PipelineProcessor/memoryAccess.v
+++ PipelineProcessor/memoryAccess.v
@@ -1,15 +1,20 @@
-module memoryAccess(instruction, address, writeData, memOut, addressOut, instructionPropagation);
-    input [19:0] address;
+module memoryAccess(instruction, address, writeData, writeEnable, writeEnable_Out, writeData_Out, address_Out, instructionPropagation);
     input [19:0] instruction;
+    input [19:0] address;
     input [19:0] writeData;
-    output [19:0] memOut;
+    input writeEnable;
+
+    output writeEnable_Out;
+    output [19:0] writeData_Out;
     output [19:0] addressOut;
-    output [19:0] instructionPropagation; // ainda é necessário?
+    output [19:0] instructionPropagation; // ainda é necessário? Sim.
 
 
     //module memory_main (addr, data, wr_en, Clock, q);
-    memory_main memory(address,writeData,writeEnable,clock,memOut);
+    //memory_main memory(address,writeData,writeEnable,clock,memOut);
     
     assign addressOut = address;
-    assign instructionPropagation = instruction; 
+    assign instructionPropagation = instruction;
+    assign writeEnable_Out = writeEnable;
+    assign writeData_Out = writeData;
 endmodule
