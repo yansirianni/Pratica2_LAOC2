ff341814d2fa0ffbefdebc83fe8c627f08790842
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-05-14 17:34:29 -0300

    Fix memoryAccess_Control

--- PipelineProcessor/memoryAccess_Control.v
+++ PipelineProcessor/memoryAccess_Control.v
@@ -1,7 +1,7 @@
 module memoryAccess_Control(instruction, writeEnable_Out);
 
     input [19:0] instruction;
-    out writeEnable_Out;
+    output writeEnable_Out;
 
     assign writeEnable_Out = instruction[19:16] == 4'b1100 ? 1 : 0; //If instruction is STORE set 1 in writeEnable_Out
 endmodule // memoryAccess_Control
