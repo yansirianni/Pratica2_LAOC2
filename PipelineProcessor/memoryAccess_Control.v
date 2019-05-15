bf0cccf259fbc20f05a451446c59f1a70a846f2e
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-05-14 22:57:53 -0300

    Add address control in memory access

--- PipelineProcessor/memoryAccess_Control.v
+++ PipelineProcessor/memoryAccess_Control.v
@@ -1,7 +1,9 @@
-module memoryAccess_Control(instruction, writeEnable_Out);
+module memoryAccess_Control(instruction, writeEnable_Out, address_Control_Out);
 
     input [19:0] instruction;
     output writeEnable_Out;
+    output address_Control_Out;
 
-    assign writeEnable_Out = instruction[19:16] == 4'b1100 ? 1 : 0; //If instruction is STORE set 1 in writeEnable_Out
+    assign writeEnable_Out = instruction[19:16] == 4'b1100 ? 1'b1 : 1'b0; //If instruction is STORE set 1 in writeEnable_Out
+    assign address_Control_Out = instruction[19:16] == 4'b1111 ? 1'b1 : 1'b0 //If instruction is COPY INPUT set 1 in address_Control_Out
 endmodule // memoryAccess_Control
