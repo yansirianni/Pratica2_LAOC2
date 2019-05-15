c653b9e579b0fe5a3d927f14d15c67b0debd3465
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-05-14 23:57:56 -0300

    Fixed register_MEM_WB

--- PipelineProcessor/register_MEM_WB.v
+++ PipelineProcessor/register_MEM_WB.v
@@ -1,23 +1,22 @@
-module register_MEM_WB(instruction,clock, reset,opcode,aluRESULT,memory_read_data,aluRESULTout,memory_read_data_out,instructionPropagation);
-
+module register_MEM_WB(instruction,clock,reset,aluRESULT,memory_read_data,aluRESULTout,memory_read_data_out,instructionPropagation);
+	input [19:0] instruction;
 	input clock, reset;
-	input [3:0] opcode;
 	input [19:0]aluRESULT;
 	input [19:0] memory_read_data; //saída da memória
-	input [19:0] instruction;
 	output reg [19:0] aluRESULTout; //saída da ula
 	output reg [19:0] memory_read_data_out;
-	output [19:0] instructionPropagation;
+	output reg [19:0] instructionPropagation;
 	
 	always @(posedge clock) begin
 		if (reset) begin
 			aluRESULTout <= 20'b0;
 			memory_read_data_out <= 20'b0;
+			instructionPropagation <= 20'b0;
 		end else begin
 			aluRESULTout <= aluRESULT;
 			memory_read_data_out <= memory_read_data;
+			instructionPropagation = instruction;
 		end
 	end
 
-	assign instructionPropagation = instruction;
 endmodule
