1cd1582952a4ffd3812b552842847849d495f86b
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-05-14 23:58:54 -0300

    Add more outputs varibles

--- PipelineProcessor/writeBack.v
+++ PipelineProcessor/writeBack.v
@@ -1,10 +1,18 @@
-module writeBack(memoryReadData,address, muxControl, writeBackOut);
+module writeBack(instruction,aluRESULTout,memoryReadData,address,writeBackEnable,writeBackAddress_Out,writeBackData_Out);
+  input [19:0] instruction;
+  input [19:0] aluRESULTout;
   input [19:0] memoryReadData;
   input [19:0] address;
-  output [19:0] writeBackOut;
-  //output [19:0] writeAdress;
+  output writeBackEnable;
+  output [19:0] writeBackAddress_Out;
+  output [19:0] writeBackData_Out;
 
-assign writeBackOut = (muxControl) ? memoryReadData : address);
+  wire [3:0] opcode;
+  assign opcode = instruction[19:16];
+
+assign writeBackEnable = opcode == 4'b1100 ? 1'b0 : 1'b1;
+assign writeBackData_Out = ((opcode == 4'b1111 || opcode == 4'b1101) ? memoryReadData : aluRESULTout);
+assign writeBackAddress_Out = address;
     
 
 endmodule
