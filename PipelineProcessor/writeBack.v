aa0dc59d00f6119f7a8bb3054a40c5276ddcb447
Heitor Amaral <heitor.amaral.santos@gmail.com>
2019-05-16 20:37:38 -0300

    Tested Conditional Copy

--- PipelineProcessor/writeBack.v
+++ PipelineProcessor/writeBack.v
@@ -11,8 +11,8 @@ module writeBack(reset,instruction,dataRFOut1,dataRFOut2,aluRESULTout,memoryRead
   wire [3:0] opcode;
   assign opcode = instruction[19:16];
 
-  assign writeBackEnable = (reset ? 1'b0 : (opcode == 4'b1100 ? 1'b0 : 1'b1));
-  assign writeBackData_Out = (reset ? 20'b0 : ((opcode == 4'b1111 || opcode == 4'b1101) ? memoryReadData : (opcode == 4'b1110 ? dataRFOut1 : (aluRESULTout))));
+  assign writeBackEnable = (reset ? 1'b0 : ((opcode == 4'b1100 || (opcode == 4'b1011 && aluRESULTout != 20'b0)) ? 1'b0 : 1'b1));
+  assign writeBackData_Out = (reset ? 20'b0 : ((opcode == 4'b1111 || opcode == 4'b1101) ? memoryReadData : (opcode == 4'b1110 ? dataRFOut1 : ((opcode == 4'b1011 && aluRESULTout == 20'b0) ? dataRFOut1 : (aluRESULTout)))));
   assign writeBackAddress_Out = (reset ? 4'b0 : (address));
     
 
